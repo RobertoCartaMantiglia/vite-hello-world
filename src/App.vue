0695bd485b62538e02870919ffca04e3e421244f
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-12-23 17:09:50 +0100

    add h1 title

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import AppTitleVue from './components/AppTitle.vue';
 </script>
+
+components: {
+  AppTitle
+}
 
 <template>
   <div>
@@ -12,6 +17,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <AppTitleVue />
 </template>
 
 <style scoped>
@@ -20,9 +26,11 @@ import HelloWorld from './components/HelloWorld.vue'
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+
+
+<template>
+    <h1>La mia prima app con vite!</h1>
+</template>
+
+<style scoped>
+
+</style>
